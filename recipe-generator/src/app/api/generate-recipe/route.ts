96534108ba9d1402a96f9218bef96b373c35851a
hashtutorial <l227753@lhr.nu.edu.pk>
2025-07-31 02:35:18 +0500

    optimization

--- recipe-generator/src/app/api/generate-recipe/route.ts
+++ recipe-generator/src/app/api/generate-recipe/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
       content: input
     };
 
-    const webhookRes = await fetch('http://localhost:5678/webhook/recipe-generator', {
+    const webhookRes = await fetch('https://l227753.app.n8n.cloud/webhook-test/chefgpt', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
